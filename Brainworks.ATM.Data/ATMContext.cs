ead9d06d4903d872c8c3b946374d59f47c6152a9
Rani Kale <ranikl1991@gmail.com>
2020-10-18 23:48:27 +0530

    Add project files.

---
+++ Brainworks.ATM.Data/ATMContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brainworks.ATM.Data
+{
+    public class ATMContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionstring = @"Data Source=DESKTOP-MHQ1JL8\SQLEXPRESS; Initial catalog=BAM; Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+    }
+}
